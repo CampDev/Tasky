b92a5ecb4598bb22be782f976ff1a6e422dae8b0
Cacauu <achternbosch@me.com>
2013-07-13 14:54:38 +0200

    Updated developer ressources

--- developer.php
+++ developer.php
@@ -1,8 +1,18 @@
 <html>
 	<head>
 		<title>Developer Ressources - Tasky</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<script src="https://gist.github.com/Cacauu/24ef6e73bb5b12a66007.js"></script>
+		<?php
+			require_once('markdown.php');
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'https://gist.github.com/Cacauu/24ef6e73bb5b12a66007/raw/9d9271b741475c478e5404de73f61258ee9cf372/posts_types.md');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$markdown = curl_exec($ch);
+			curl_close($ch);
+			$html = Markdown($markdown);
+			echo $html;
+		?>
 	</body>
 </html>
