d0e4d18884c97dd5ce2f725203a6a09afb01ac53
Cacauu <achternbosch@me.com>
2013-07-13 14:40:35 +0200

    Added developer ressources

---
+++ developer.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Developer Ressources - Tasky</title>
+	</head>
+	<body>
+		<script src="https://gist.github.com/Cacauu/24ef6e73bb5b12a66007.js"></script>
+	</body>
+</html>
