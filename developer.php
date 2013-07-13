e86d8dcc18915b68001cae26c50bf1e3e7b6f602
Cacauu <achternbosch@me.com>
2013-07-13 14:59:19 +0200

    Changed Developer Ressources from Gist to plain text in the code

--- developer.php
+++ developer.php
@@ -6,11 +6,41 @@
 	<body>
 		<?php
 			require_once('markdown.php');
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'https://gist.github.com/Cacauu/24ef6e73bb5b12a66007/raw/9d9271b741475c478e5404de73f61258ee9cf372/posts_types.md');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$markdown = curl_exec($ch);
-			curl_close($ch);
+			$markdown = '#Tasky Post Type Documentation
+
+Tasky uses two custom post types to store tasks and lists in your Tent account. These two post types are, of course, available for every other app to and documented below:
+
+(Required parameters are bold, optional parameters are regular)
+
+##Task 
+####URL: http://cacauu.de/tasky/task/v0.1
+
+* **Title (required) - Title describing the task**
+
+* **Status (required) - Status of the post *(Possible: Done, Due)***
+
+* Priority (optional) - Priority of the task *(Possible in Tasky: 0 (low), 1 (average), 2 (high), 3 (urgent); Default in Tasky: 1 (average))*
+
+* List (optional) - List for the task *(Default in Tasky: To Do)*
+
+* Tags (optional) - Tags for the task
+
+* Assigned User - (optional) - User who is assigned to *do* the task *(In the JSON: Assignee)*
+
+* Notes (optional) - Additional notes about the task
+
+* Due Date (optional) - Unix epoch of date (and time) the task is due *(In the JSON: duedate)*
+
+* Public (optional) - Public or private task *(Possible: True or False, managed through Tent permissions)*
+
+* Group (optional) - Users who are allowed to view the task *(Only for private tasks, managed through Tent mentions)*
+
+##Task List 
+####URL: http://cacauu.de/tasky/list/v0.1
+
+* **Name (required) - Title of the list, brief description**
+
+* Description (optional) - Description of the list';
 			$html = Markdown($markdown);
 			echo $html;
 		?>
