f76435118235663a27c0845e4933c49d6956ad76
kevinkleinman <kevin_kleinman@live.nl>
2013-07-26 16:15:48 +0000

    footer.php edited online with Bitbucket

--- footer.php
+++ footer.php
@@ -2,4 +2,6 @@
         <div id="footer">
         Created by <a href="https://cacauu.tent.is">^Cacauu</a> - <a href="developer.php">Developer Resources</a> - <a href="logout.php">Logout</a>
         </div>
+        <div id="new_post" style="display: none;"><?php include('new_post_page.php') ?></div>
+        <div id="list_management" style="display: none;"><?php include('list.php') ?></div>
 		</footer>
