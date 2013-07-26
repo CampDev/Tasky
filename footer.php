d33ae126ed22c29c3fc81723533e11b72a0ad59d
kevinkleinman <kevin_kleinman@live.nl>
2013-07-26 09:46:35 +0000

    Logout back in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<footer>
         <div id="footer">
-        Created by <a href="https://cacauu.tent.is">^Cacauu</a> - <a href="developer.php">Developer Resources</a>
+        Created by <a href="https://cacauu.tent.is">^Cacauu</a> - <a href="developer.php">Developer Resources</a> - <a href="logout.php">Logout</a>
         </div>
 		</footer>
