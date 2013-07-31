ca2a4e46d2aef64d75534b55e8686590f454d019
kevinkleinman <kevin_kleinman@live.nl>
2013-07-31 17:18:16 +0000

    Woohoo! CampDev! :p

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<footer>
         <div id="footer">
-        Created by <a href="https://cacauu.tent.is">^Cacauu</a> - <a href="developer.php">Developer Resources</a> - <a href="logout.php">Logout</a>
+        Developed by <a href="http://dev.campnews.org">^CampDev</a> - <a href="developer.php">Developer Resources</a> - <a href="logout.php">Logout</a>
         </div>
         <div id="new_post" style="display: none;"><?php include('new_post_page.php') ?></div>
         <div id="list_management" style="display: none;"><?php include('list.php') ?></div>
