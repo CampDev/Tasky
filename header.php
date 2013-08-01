790550b1400bd160cfbd997679f7b6bdba65745e
Cacauu <achternbosch@me.com>
2013-08-01 20:49:01 +0200

    Added title for header buttons

--- header.php
+++ header.php
@@ -40,8 +40,8 @@
                     } ?>
                 </select>
 <div class="header-navigation">
-                <a class="javascript-nav" rel="leanModal" href="#new_post"><img src="img/createpost.png" style="margin-left: 20px; width: 28px;" alt="New post"></a>
-                <a class="javascript-nav" rel="leanModal" href="#list_management"><img src="img/list.png" style="margin-left: 20px; width: 28px;" alt="List management"></a>
+                <a class="javascript-nav" title="Manage Lists" rel="leanModal" href="#new_post"><img src="img/createpost.png" style="margin-left: 20px; width: 28px;" alt="New post"></a>
+                <a class="javascript-nav" title="New Task" rel="leanModal" href="#list_management"><img src="img/list.png" style="margin-left: 20px; width: 28px;" alt="List management"></a>
                 <a class="javaless-nav" href="new_post_page.php"><img src="img/createpost.png" style="margin-left: 20px; width: 28px;" alt="New post"></a>
                 <a class="javaless-nav" href="list.php"><img src="img/list.png" style="margin-left: 20px; width: 28px;" alt="List management"></a>
 <!-- <img src="" style="width: 40px; height: 40px; margin-top: -5px; float: right;"> -->
