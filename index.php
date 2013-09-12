e1bdbedc0c8b2ae9e8574cdb78b797ca00bbbd6d
Cacauu <achternbosch@me.com>
2013-09-12 21:57:31 +0200

    Major cleanup in index.php

--- index.php
+++ index.php
@@ -50,8 +50,8 @@ require_once('tent-markdown.php');
 				<?php
 				if (!isset($_GET['list'])) {
 					unset($_SESSION['redirect_list']);
-					if (!isset($_GET['filter'])) {
-						$url = $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1';	
+					if (isset($_GET['filter'])) {
+						$url = $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1&mentions='.urlencode($_SESSION['entity']).'+'.$_GET['list'];	
 					}
 					elseif ($_GET['filter'] == 'todo') {
 						$url = $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1%23todo';
@@ -61,6 +61,9 @@ require_once('tent-markdown.php');
 					}
 					elseif ($_GET['filter'] == 'test') {
 						$url = $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1%23test';
+					}
+					else {
+						$url = $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1';
 					}
 					$mac = generate_mac('hawk.1.header', time(), $nonce, 'GET', str_replace($entity, "/", $url), $entity_sub, '443', $_SESSION['client_id'], $_SESSION['hawk_key'], false);
 					$init = curl_init();
@@ -74,7 +77,7 @@ require_once('tent-markdown.php');
 
                     /* Welcome page */
 
-					if (!isset($_GET['filter']) AND $posts['posts'] == array()) { ?>
+					if (!isset($_GET['filter']) AND !isset($_GET['list']) AND $posts['posts'] == array()) { ?>
 					<div class="filters">Tasks</div>
 						<h2>Welcome to Tasky</h2>
                         <div style="text-align: center;">
@@ -83,6 +86,10 @@ require_once('tent-markdown.php');
 						<p>2. <a href="new_post_page.php">Start adding tasks.</a></p>
 						<p>3. Click on "All lists" to filter by list.</p>
                     </div><?php }
+
+                    elseif (isset($_GET['list']) AND $posts['posts'] == array()) { ?>
+                    	<h2>No tasks in <?php echo $current_list['post']['content']['name']; ?>. <a href="new_post_page.php">Add one!</a></h2>
+                    <?php }
 
                     /* Tasks from all lists */
 
@@ -133,87 +140,7 @@ require_once('tent-markdown.php');
 						<?php }
 					} ?>
 					</div>
-				<div class='clear'></div><?php }
-
-                    /* Tasks from chosen list */
-
-				elseif (isset($_GET['list'])) {
-					$_SESSION['redirect_list'] = $_GET['list'];
-					$id = $_GET['list'];
-					$entity_sub_list = substr_replace($_SESSION['entity'] ,"",-1);
-					$current_url = str_replace("{entity}", urlencode($entity_sub_list), $_SESSION['single_post_endpoint']);
-					$current_url = str_replace("{post}", $id, $current_url);
-					$mac_current = generate_mac('hawk.1.header', time(), $nonce, 'GET', str_replace($_SESSION['entity'], "/", $current_url), $_SESSION['entity_sub'], '443', $_SESSION['client_id'], $_SESSION['hawk_key'], false);
-					$ch_current = curl_init();
-					curl_setopt($ch_current, CURLOPT_URL, $current_url);
-					curl_setopt($ch_current, CURLOPT_RETURNTRANSFER, 1);
-					curl_setopt($ch_current, CURLOPT_HTTPHEADER, array(generate_auth_header($_SESSION['access_token'], $mac_current, time(), $nonce, $_SESSION['client_id'])));
-					$current_list = curl_exec($ch_current);
-					curl_close($ch_current);
-					$current_list = json_decode($current_list, true);
-
-					//Getting tasks from the chosen list
-					$mac = generate_mac('hawk.1.header', time(), $nonce, 'GET', str_replace($_SESSION['entity'], "/", $_SESSION['posts_feed_endpoint']).'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1&mentions='.urlencode($_SESSION['entity']).'+'.$_GET['list'], $entity_sub, '443', $_SESSION['client_id'], $_SESSION['hawk_key'], false);
-					$init = curl_init();
-					curl_setopt($init, CURLOPT_URL, $_SESSION['posts_feed_endpoint'].'?types=http%3A%2F%2Fcacauu.de%2Ftasky%2Ftask%2Fv0.1&mentions='.urlencode($_SESSION['entity']).'+'.$_GET['list']);
-					curl_setopt($init, CURLOPT_HTTPGET, 1);
-					curl_setopt($init, CURLOPT_RETURNTRANSFER, 1);
-					curl_setopt($init, CURLOPT_HTTPHEADER, array(generate_auth_header($_SESSION['access_token'], $mac, time(), $nonce, $_SESSION['client_id']))); //Setting the HTTP header
-					$posts = curl_exec($init);
-					curl_close($init);
-					$posts = json_decode($posts, true);
-					if ($posts['posts'] != array()) {
-						foreach ($posts['posts'] as $task) {
-							$content = $task['content'];?>
-							<div id='single-task' class='<?php echo strtolower($content['status']); ?>'>
-
-							<?php if (isset($content['status']) AND $content['status'] == 'To Do' OR $content['status'] == 'todo') { ?>
-								<a href='task_handler.php?type=complete&id=<?php echo $task['id']; ?>&parent=<?php echo $task['version']['id']; ?>'><img class="priority" src="img/checkbox_<?php echo $content['priority']; ?>.svg" /></a>
-							<?php }
-							elseif (isset($content['status']) AND $content['status'] == 'Done' OR $content['status'] == 'done') { ?>
-								<a href='task_handler.php?type=uncomplete&id=<?php echo $task['id']; ?>&parent=<?php echo $task['version']['id']; ?>'><img class="priority" src="img/checkbox_done.svg"></a>
-
-							<?php }
-							else {
-								echo "";
-							} ?>
-
-							<div class='task-body'>
-								<div class='title'><a class='edit' href='edit.php?type=update&id=<?php echo $task['id']; ?>'><?php echo $content['title']; ?></div>
-
-							<?php if ($content['notes'] != '' AND !is_null($content['notes'])) { ?>
-								<i><div class='note'><?php echo Tent_Markdown($content['notes']); ?></div></i></a></div>
-							<?php }
-							else { ?>
-								</div>
-							<?php }
-								if (isset($content['duedate']) AND $content['duedate'] != '') {
-								if (date('d/M/Y', $content['duedate']) == date('d/M/Y', time())) { ?>
-									<div class='date'>Today</div>
-								<?php }
-								elseif ($content['duedate'] < time() and strtolower($content['status']) != 'done') { ?>
-									<div class='date' style='color: red;'><?php echo date('d/M/Y', $content['duedate']); ?></div>
-								<?php }
-								else { ?>
-									<div class='date'><?php echo date('d/M/Y', $content['duedate']); ?></div>
-								<?php }
-							}
-							else {
-								echo "";
-							} ?>
-
-							<a href='task_handler.php?type=delete&id=<?php echo $task['id']; ?>'><img class='delete' src="img/delete.svg"></a>
-
-							<?php echo "</div>";
-						}
-						echo "</div>";
-					}
-					else { ?>
-						<h2>No tasks in <?php echo $current_list['post']['content']['name']; ?>. <a href="new_post_page.php">Add one!</a></h2>
-					<?php }
-				}
-				?>
-                <div class='clear'></div>
+				<div class='clear'></div><?php } ?>
 
 		<?php include('footer.php') ?>
 
