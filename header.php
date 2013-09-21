a9644158a6d988499258bfa9ac78da0ceda1de7e
nygu <kevin_kleinman@live.nl>
2013-09-21 15:06:32 +0200

    SVGs in header cleaned up, recoloured menu icon

--- header.php
+++ header.php
@@ -34,8 +34,6 @@
         }
         //-->
         </script>
-
-        <link rel="icon" type="image/ico" href="favicon.ico" />
 </head>
 
         <div class="header">
@@ -64,41 +62,11 @@
                     } ?>
                 </select>
 <div class="header-navigation">
-<a href="#" onclick="toggle_visibility('mobile-menu');"><img class="menu-button" src="img/menu.svg"></a>
-
-                <a class="javascript-nav" title="New task" rel="leanModal" href="#new_post">
-<svg alt="New post" version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
-	 width='36px' height='36px' viewBox='0 0 512 512' enable-background='new 0 0 512 512' xml:space='preserve'>
-<path style='fill: #ffffff;' id='note-25-icon' d='m 369.162,49.667 c -45.725,0 -82.794,37.066 -82.794,82.791 0,45.725 37.069,82.791 82.794,82.791 45.723,0 82.79,-37.066 82.79,-82.791 0,-45.725 -37.067,-82.791 -82.79,-82.791 z m 50.881,97.834 -35.837,0 0,35.838 -27.678,0 0,-35.838 -35.837,0 0,-27.676 35.837,0 0,-35.836 27.678,0 0,35.836 35.837,0 z m -0.661,86.01 0,78.352 c 0,61.402 -84.153,150.471 -152.318,150.471 l -207.016,0 0,-412 231.806,0 c -0.824,0.777 -1.643,1.564 -2.447,2.369 -10.931,10.93 -19.334,23.711 -24.934,37.631 l -164.425,0 0,332 c 118.342,0 134.344,0 166.406,0 58.859,0 35,-76.092 35,-76.092 0,0 77.928,25.289 77.928,-37.832 0,-9.807 0,-34.365 0,-63.621 14.057,-1.257 27.576,-5.091 40,-11.278 z'
-	/>
-</svg></a>
-                <a class="javascript-nav" title="List management" rel="leanModal" href="#list_management">
-
-<svg alt="List management" version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
-	 width='36px' height='36px' viewBox='0 0 512 512' enable-background='new 0 0 512 512' xml:space='preserve'>
-<path style='fill: #ffffff;' id='note-17-icon' d='M370.845,339.166H209.821v-30h161.023V339.166z M370.845,280.749H209.821v-30h161.023V280.749z
-	 M370.845,223.416H209.821v-30h161.023V223.416z M170.166,421.825V156.714H409.5c0,0,0,133.5,0,165.25
-	c0,50.953-70.109,33.833-70.109,33.833s16.609,66.028-32,66.028C275.328,421.825,288.508,421.825,170.166,421.825z M449.5,320.417
-	V116.714H130.166v345.111H308C376.165,461.825,449.5,381.819,449.5,320.417z M97.5,420.942V85.333h311V50.175h-346v370.768H97.5z'/>
-</svg>
-
-</a>
-                <a class="javaless-nav" href="new_post_page.php">
-<svg alt="New post" version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
-	 width='36px' height='36px' viewBox='0 0 512 512' enable-background='new 0 0 512 512' xml:space='preserve'>
-<path style='fill: #ffffff;' id='note-25-icon' d='m 369.162,49.667 c -45.725,0 -82.794,37.066 -82.794,82.791 0,45.725 37.069,82.791 82.794,82.791 45.723,0 82.79,-37.066 82.79,-82.791 0,-45.725 -37.067,-82.791 -82.79,-82.791 z m 50.881,97.834 -35.837,0 0,35.838 -27.678,0 0,-35.838 -35.837,0 0,-27.676 35.837,0 0,-35.836 27.678,0 0,35.836 35.837,0 z m -0.661,86.01 0,78.352 c 0,61.402 -84.153,150.471 -152.318,150.471 l -207.016,0 0,-412 231.806,0 c -0.824,0.777 -1.643,1.564 -2.447,2.369 -10.931,10.93 -19.334,23.711 -24.934,37.631 l -164.425,0 0,332 c 118.342,0 134.344,0 166.406,0 58.859,0 35,-76.092 35,-76.092 0,0 77.928,25.289 77.928,-37.832 0,-9.807 0,-34.365 0,-63.621 14.057,-1.257 27.576,-5.091 40,-11.278 z'
-	/>
-</svg>
-</a>
-                <a class="javaless-nav" href="list.php">
-<svg alt="List management" version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
-	 width='36px' height='36px' viewBox='0 0 512 512' enable-background='new 0 0 512 512' xml:space='preserve'>
-<path style='fill: #ffffff;' id='note-17-icon' d='M370.845,339.166H209.821v-30h161.023V339.166z M370.845,280.749H209.821v-30h161.023V280.749z
-	 M370.845,223.416H209.821v-30h161.023V223.416z M170.166,421.825V156.714H409.5c0,0,0,133.5,0,165.25
-	c0,50.953-70.109,33.833-70.109,33.833s16.609,66.028-32,66.028C275.328,421.825,288.508,421.825,170.166,421.825z M449.5,320.417
-	V116.714H130.166v345.111H308C376.165,461.825,449.5,381.819,449.5,320.417z M97.5,420.942V85.333h311V50.175h-346v370.768H97.5z'/>
-</svg>
-</a>
+    <a href="#" onclick="toggle_visibility('mobile-menu');"><img class="menu-button" src="img/menu.svg"></a>
+    <a class="javascript-nav" title="New task" rel="leanModal" href="#new_post"><img class="nav-icon" src="img/create-task.svg"></a>
+    <a class="javascript-nav" title="List management" rel="leanModal" href="#list_management"><img class="nav-icon" src="img/lists.svg"></a>
+    <a class="javaless-nav" href="new_post_page.php"><img class="nav-icon" src="img/create-task.svg"></a>
+    <a class="javaless-nav" href="list.php"><img class="nav-icon" src="img/lists.svg"></a>
                 <?php } ?>
 </div>
 
