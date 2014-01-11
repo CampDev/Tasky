2044d24ef048e808c33e6d0077daf189a482f2e3
Cacauu <achternbosch@me.com>
2014-01-11 10:03:28 +0100

    Add landing page
    Needs some more cleaning and polishing

--- src/router.php
+++ src/router.php
@@ -4,5 +4,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app->get('/', function() use ($app) {
-	return $app['twig']->render('index.twig');
+	if (isset($_SESSION['entity'])) {
+		return $app['twig']->render('index.twig', array(
+		'authed_user' => $_SESSION['entity']
+	));	
+	}
+	else {
+		return $app['twig']->render('landing.twig', array());
+	}
 })->bind('index');
+
+$app->get('/auth/{entity}', function($entity) use ($app) {
+	return 'Lets authenticate you...';
+});
